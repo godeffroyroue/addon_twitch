419121532bf54dba3becaf1abda8e1782579dfae
Godeffroy <godeffroy.roue@epitech.eu>
2020-06-05 03:01:19 +0200

    [-] delete all

--- api/controllers/Controllers.js
+++
@@ -1,5 +0,0 @@
-// const { check, validationResult } = require('express-validator');
-
-exports.home = function(req, res) {
-    res.send(200).json({"message" : "Welcome on the AddON Gveriz API"})
-};
